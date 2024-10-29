59ec5a512b2f191bb079e53d180e290771420568
vkSegfault <adtofaust@gmail.com>
2024-10-29 22:58:11 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,58 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    // TODO
+    // move to below once dotnet 9 is released
+    // https://www.nuget.org/packages/Scalar.AspNetCore
+    // https://www.youtube.com/watch?v=8yI4gD1HruY
+    // app.MapOpenApi();
+    
+    // swagger is no longer included in dotnet 9
+    app.UseSwagger(options =>
+        {
+            options.RouteTemplate = "openapi/{documentName}/openapi.json";
+        });
+    // app.UseSwaggerUI();
+    
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
